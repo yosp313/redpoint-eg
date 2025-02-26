12cf571f607a040e73a9ba48a1f313716b5f5d77
Youssef Ayman <yosp31@gmail.com>
2025-02-26 23:38:33 +0200

    white mode as the default

--- config/site.ts
+++ config/site.ts
@@ -29,7 +29,7 @@ const baseSiteConfig = {
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
-  nextThemeColor: "dark", // next-theme option: system | dark | light
+  nextThemeColor: "white", // next-theme option: system | dark | light
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
